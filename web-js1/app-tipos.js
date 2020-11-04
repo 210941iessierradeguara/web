d16cf45d06ff62c4b4d26b5855df49423060e166
Daniel <210941@iessierradeguara.com>
2020-11-04 17:16:22 +0100

    Clase online 04/11/20 Java1

---
+++ web-js1/app-tipos.js
@@ -0,0 +1,18 @@
+// alert("Hello world");
+// alert("Esto es una cadena de texto");
+// alert("esto también es una cadena de txto");
+// alert("I'm the teacher");
+// alert('I\'m the teacher');
+
+// alert(2+2);
+// alert(2-3);
+// alert(2*3);
+// alert(2/3);
+
+// alert(true && true);
+// alert(false || false);
+
+let mensaje="Hola mundo";
+let nombre="Daniel";
+alert(mesaje);
+alert("Hola "+nombre);
---
+++ web-js1/app.js
@@ -0,0 +1,25 @@
+/* 
+let v1 = 3;
+let v2 = 4;
+let v3 = 6;
+alert(v1+v2+v3);
+
+let va1 = 5;
+let va2 = 2;
+let resultado = va1 * va2;
+alert(resultado);
+ */
+
+/* 
+ alert("Mira el h1");
+ let elemento = document.getElementById("encabezado");
+ elemento.innerHTML="Hola clase";
+  */
+
+let resultado;
+
+ resultado = confirm("¿Quieres que te salude");
+
+ if (resultado == true) {
+     alert("Hola");
+ }
