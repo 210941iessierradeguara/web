045d302ac37b2ee4b6c343704dc47e510cebaca5
Daniel <210941@iessierradeguara.com>
2020-11-11 17:17:14 +0100

    11/11/20

---
+++ web-js-funciones/app.js
@@ -0,0 +1,50 @@
+/* let parrafo = document.getElementById("parrafo");
+
+parrafo.innerHTML = "Adios";
+ */
+/* 
+function saludar() {
+    alert("Hola Clase");
+}
+
+saludar();
+
+function SaludoPersonalizado(nombre) {
+    let mensaje="hola ";
+    alert(mensaje+nombre);
+}
+
+function cambiarParrafo(contenido) {
+    let elemento = document.getElementById("parrafo");
+    elemento.innerHTML = contenido
+}
+
+SaludoPersonalizado("Pepe");
+SaludoPersonalizado("Isabel");
+SaludoPersonalizado("Martín");
+let nombre = "Javier";
+SaludoPersonalizado(nombre);
+
+function cambiarElemento(idElemento, conElemento) {
+    let elemeto = document.getElementById(idElemento);
+    elemento.innerHTML = conElemento;
+}
+
+cambiarParrafo("hola clase");
+alert();
+cambiarElemento("parrafo","hola cracola");
+alert();
+cambiarElemento("parrafo2", "hola mundo");
+alert();
+cambiarElemento("parrafo3", "Adiós Mundo");
+
+function ejecutaFuncion(parFuncion) {
+    parFuncion();
+}
+
+ejecutaFuncion(alert);
+ejecutaFuncion(saludar); */
+
+let input = document.getElementById("idInput");
+
+input.value = "Hello world";
