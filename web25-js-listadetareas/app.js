39e4d8bef5ddc2f2df28bd8bc0986ab4e801e152
Daniel <210941@iessierradeguara.com>
2020-11-17 17:25:53 +0100

    JS tareas 17:27 17-11-2020

--- web25-js-listadetareas/app.js
+++ web25-js-listadetareas/app.js
@@ -1,2 +1,22 @@
-let listatareas = document.getElementById("listatareas");
-listatareas.innerHTML = "<li>Hola</li>"
+function anyadirTarea() {
+    //tomo el valor de input
+    let eTarea = document.getElementById("idTarea");
+
+    //compruebo si tiene algo de texto
+    if (eTarea.value != "") {
+
+        //si tiene texto, añado el elemento en la lista
+        anyadirElemento(eTarea.value);
+        //despues de añadir borro el elemento input
+        eTarea.value = "";
+    }
+    //si no tiene texto, no hago nada
+}
+
+function anyadirElemento(textoTarea) {
+    //cojo el elemtento de la lista
+    let listaTareas = document.getElementById("listaTareas"); 
+    //Le añado un elemento de lista con el texto del parámetro textoTarea
+    listaTareas.innerHTML +="<li>"+textoTarea+"</li>"
+}
+
