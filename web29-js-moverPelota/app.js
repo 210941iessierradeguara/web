2ec0e0122a9176363603de2598f05b6ed01cfdcc
Daniel <210941@iessierradeguara.com>
2020-12-09 17:15:40 +0100

    9/12/2020 17:17

--- web29-js-moverPelota/app.js
+++ web29-js-moverPelota/app.js
@@ -1,16 +1,35 @@
 let bola = document.getElementById("bola")
+let vel = 5;
+let ultimaPulsacion = "";
 function moverBola(evento) {
-    console.log(evento);
+    let posX = bola.x;
+    let posY = bola.y;
+    
+    if (ultimaPulsacion == evento.key) {
+        vel++;
+    } else{
+        vel=5;
+    }
+    ultimaPulsacion = evento.key;
+    
 if (evento.key == "ArrowUp") {
-    bola.style.top=y-1+"px";
+    if (posY > 0) {
+        bola.style.top = posY - vel + "px";
+    }
 }
 else if (evento.key == "ArrowDown") {
-    bola.style.top=y+1+"px";
+    if (posY < window.innerHeight-150) {
+        bola.style.top = posY + vel + "px";    
+    }
 }
 else if (evento.key == "ArrowLeft") {
-    bola.style.top=X-1+"px";
+    if (posX > 0) {
+        bola.style.left = posX - vel + "px";    
+    }
 }
-else if (evento.key == "ArrowLeft") {
-    bola.style.top=X+1+"px";
+else if (evento.key == "ArrowRight") {
+    if (posX < window.innerWidth-150) {
+        bola.style.left = posX + vel + "px";    
+    }
 }
 }
